974d025bf229ab264b409c8ef3b56e4ca2b8dafc
Shishirthakur2416 <samadkhan040204@gmail.com>
2026-01-07 11:18:56 +0530

    * origin updated

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173", // Frontend origin
+  origin: "*", // Allow all origins
   credentials: true
 }));
 app.use(express.json());
